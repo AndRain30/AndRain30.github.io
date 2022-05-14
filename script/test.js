dd1319a2ac4ed757a6e33fa942a9dca2e9c457aa
AndRain <zai4inaa@yandex.ru>
2022-05-14 09:29:29 +0500

    добавлена анимация кнопки

---
+++ script/test.js
@@ -0,0 +1,13 @@
+let antworts = document.querySelector('.test__antworts').children;
+let buttonConfirm = document.querySelector('.test__button-confirm');
+let clickAntwort = document.querySelector('.test__antwort');
+
+for (let i = 0; i < clickAntwort.children.length; i++) {
+    clickAntwort.addEventListener('click', () => {
+        ad
+    })
+}
+
+buttonConfirm.addEventListener('click', (antworts) => {
+    if (antworts.)
+})
